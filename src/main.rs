f6adced9da5b7aa9681378c8e1d3ff6507bc21f8
Aysuka Ansari <aysukaansari@apps.ipb.ac.id>
2023-05-26 02:22:47 +0700

    mengimplementasikan BigInt dan menambahkan fungsi totient

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-use std::{collections::HashSet};
+use std::collections::HashSet;
+use num_bigint::BigInt;
+use num_traits::ToPrimitive;
 
-fn is_prime(n: &u32) -> bool {
-    if *n <= 3 {
+fn is_prime(n: &BigInt) -> bool {
+    // Ganti ke miller rabin test
+    if *n <= BigInt::from(3) {
         return false;
     }
 
-    if (*n % 2 == 0) || (*n % 3 == 0) {
+    if (n % BigInt::from(2) == BigInt::from(0)) || (n % BigInt::from(3) == BigInt::from(0)) {
         return false;
     }
 
-    let mut i: u32 = 5;
-    while i * i <= *n {
-        if (*n % i == 0) || (*n % i + 2) == 0 {
+    let mut i: BigInt = BigInt::from(5);
+    while &i * &i <= *n {
+        if (n % &i == BigInt::from(0)) || (n % &i + 2) == BigInt::from(0) {
             return false;
         }
         i += 6;
@@ -20,50 +23,48 @@ fn is_prime(n: &u32) -> bool {
     true
 }
 
-fn prime_set(v: &Vec<u32>) -> HashSet<u32> {
-    let set = v.iter().cloned().collect();
+fn prime_set(v: &Vec<BigInt>) -> HashSet<BigInt> {
+    let set: HashSet<BigInt> = v.iter().cloned().collect();
     set
 }
 
-fn prime_factors(n: &u32) -> HashSet<u32> {
+fn prime_factors(n: &BigInt) -> Vec<BigInt> {
     let mut factors = Vec::new();
-    let mut num = *n;
-    let mut divisor = 2;
-
-    while divisor * divisor <= num {
-        if num % divisor == 0 {
-            factors.push(divisor);
-            num /= divisor;
+    let mut num = n.clone();
+    let mut divisor = BigInt::from(2);
+    
+    while &divisor * &divisor <= num {
+        if &num % &divisor == BigInt::from(0) {
+            factors.push(divisor.clone());
+            num /= &divisor;
         }
         else {
             divisor += 1;
         }
     }
 
-    if num > 1 {
+    if &num > &BigInt::from(1) {
         factors.push(num);
     }
 
-    let set_factors: HashSet<u32> = prime_set(&factors);
-
-    set_factors
+    factors
 }
 
-fn primitive_root_modulo(n: &u32) -> u32 {
-    if *n < 5 {
-        return 2;
+fn primitive_root_modulo(n: &BigInt) -> BigInt {
+    if *n < BigInt::from(5) {
+        return BigInt::from(2);
     }
 
-    // asumsi n adalah prima
-    let torsion_n: u32 = *n - 1;
-    let set_factors: HashSet<u32> = prime_factors(&torsion_n);
+    let totient: BigInt = n.clone() - BigInt::from(1);
+    let set_factors: HashSet<BigInt> = prime_set(&prime_factors(&totient));
 
-    for g in 2..*n {
+    let mut g = BigInt::from(2);
+    while g < *n {
         let mut is_primitive_root = false;
         for element in &set_factors {
-            let exp: u32 = torsion_n / element;
-            let result: u32 = powmod(&g, &exp, &n);
-            if result == 1 {
+            let exp = &totient / element;
+            let result = powmod(&g, &exp, &n);
+            if result == BigInt::from(1) {
                 is_primitive_root = true;
                 break;
             }
@@ -71,42 +72,53 @@ fn primitive_root_modulo(n: &u32) -> u32 {
         if is_primitive_root {
             return g;
         }
+        g += 1;
     }
 
     unreachable!();
 }
 
-fn powmod(a: &u32, b: &u32, p: &u32) -> u32 {
-    let mut result = 1;
-    let mut base = *a % *p;
-    let mut exp = *b;
+fn powmod(a: &BigInt, b: &BigInt, p: &BigInt) -> BigInt {
+    let mut result = BigInt::from(1);
+    let mut base = a % p;
+    let mut exp = b.clone();
 
-    while exp > 0 {
-        if exp % 2 == 1 {
-            result = (result * base) % p;
+    while &exp > &BigInt::from(0) {
+        if &exp % &BigInt::from(2) == BigInt::from(1) {
+            result = (result * &base) % p.clone();
         }
-        base = (base * base) % *p;
-        exp /= 2;
+        base = (&base * &base) % p.clone();
+        exp /= BigInt::from(2);
     }
 
     result
 }
 
-fn main() {
-    let p: u32 = 41;
-    if is_prime(&p) {
-        let g: u32 = primitive_root_modulo(&p);
-        let private_a: u32 = 4;
-        let private_b: u32 = 3;
+fn euler_totient(n: &BigInt) -> BigInt {
+    let mut result = n.clone();
+    let prime_factors_set = prime_set(&prime_factors(&n));
+    for element in &prime_factors_set {
+        result -= &result / element;
+    }
+    result
+}
 
-        let public_a: u32 = powmod(&g, &private_a, &p);
-        let public_b: u32 = powmod(&g, &private_b, &p);
+fn main() {
+    let p = BigInt::from(17);
+    if is_prime(&p) {
+        let g = primitive_root_modulo(&p);
+        let private_a = BigInt::from(4);
+        let private_b = BigInt::from(3);
+
+        let public_a = powmod(&g, &private_a, &p);
+        let public_b = powmod(&g, &private_b, &p);
 
         let key_a = powmod(&public_b, &private_a, &p);
         let key_b = powmod(&public_a, &private_b, &p);
 
         if key_a == key_b {
             println!("Shared key = {}", key_a);
+            println!("Totient = {}", euler_totient(&p));
             println!("p = {}", p);
             println!("g = {}", g);
         }
