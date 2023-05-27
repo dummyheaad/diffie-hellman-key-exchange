1321062a813654b67ca5b113679836da4637a205
Aysuka Ansari <aysukaansari@apps.ipb.ac.id>
2023-05-27 19:21:10 +0700

    menambahkan modul crypto_utils

---
+++ src/crypto_utils.rs
@@ -0,0 +1,64 @@
+use curv::{BigInt, arithmetic::Converter};
+use crate::affine;
+use base64::{Engine, engine::general_purpose};
+
+pub fn encrypt_to_b64(message: &str, cryp: &affine::AffineCipher) -> String {
+    let mut ciphertext_vec: Vec<BigInt> = vec![];
+    for ch in message.chars() {
+        let msg_ascii = BigInt::from(ch as u32);
+        let c = cryp.encrypt(&msg_ascii);
+        ciphertext_vec.push(c);
+    }
+
+    let ciphertext_hex: String = ciphertext_vec
+        .iter()
+        .map(|c| {
+            let hex_c = c.to_str_radix(16);
+            if hex_c.len() == 1 {
+                format!("0{}", hex_c)
+            }
+            else {
+                hex_c
+            }
+        })
+        .collect();
+
+    let ciphertext_b64 = general_purpose::STANDARD.encode(&ciphertext_hex);
+    ciphertext_b64
+}
+
+pub fn decrypt_from_b64(cipher: &String, cryp: &mut affine::AffineCipher) -> String {
+    let decoded_bytes = general_purpose::STANDARD.decode(&cipher).unwrap();
+    let decoded_string = String::from_utf8(decoded_bytes).unwrap();
+    
+    let mut decoded_ciphertext: Vec<BigInt> = Vec::new();
+    for i in (0..decoded_string.len()).step_by(2) {
+        match &decoded_string[i..i+2].parse::<String>() {
+            Ok(hex_str) => {
+                if let Ok(num) = BigInt::from_str_radix(hex_str, 16) {
+                    decoded_ciphertext.push(num);
+                }
+            }
+            Err(e) => {
+                println!("Error {}", e);
+            }
+        }
+    }
+    
+    let mut plaintext_vec: Vec<BigInt> = vec![];
+    for dc in &decoded_ciphertext {
+        let plain = cryp.decrypt(&dc);
+        plaintext_vec.push(plain);
+    }
+
+    let plaintext: String = plaintext_vec
+    .iter()
+    .map(|p| {
+        let string = p.to_str_radix(10);
+        let value = u32::from_str_radix(&string, 10).unwrap();
+        char::from_u32(value).unwrap()
+    })
+    .collect();
+
+    plaintext
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use curv::{BigInt, arithmetic::Converter, arithmetic::Zero};
-use base64::{Engine, engine::general_purpose};
+use curv::{BigInt, arithmetic::Zero};
 
 mod prime;
 mod utils;
 mod affine;
 mod diffie_helman;
+mod crypto_utils;
 
 fn main() {
 
@@ -37,7 +37,9 @@ fn main() {
         };
 
         // Contoh Enkripsi dan Dekripsi sederhana via Affine Cipher
-        println!("=====DEKRIPSI DIFFIE-HELLMAN VIA AFFINE CIPHER=====");
+        // MASALAH: NILAI P GA BISA TERLALU GEDE.
+        // IMPROVISASI: BIKIN AGAR NILAI P NYA BISA GEDE DAN BERHASIL DEKRIPSI PESAN
+        println!("=====ENKRIPSI/DEKRIPSI DIFFIE-HELLMAN VIA AFFINE CIPHER=====");
         println!("p = {}", p);
         println!("g = {}", g);
         println!("Private Key Alice = {}", dh_alice.get_private_key());
@@ -50,69 +52,14 @@ fn main() {
         // ENKRIPSI
 
         // let message = "Selamat ulang tahun yang ke-21, Aysuka Ansari !!!";
-        let message = "I Love U So Much";
-
-        let mut message_vec: Vec<BigInt> = vec![];
-        for ch in message.chars() {
-            let message_ascii = BigInt::from(ch as u32);
-            message_vec.push(message_ascii);
-        }
-
-        let mut ciphertext_vec: Vec<BigInt> = vec![];
-        for m in &message_vec {
-            let c = affn.encrypt(&m);
-            ciphertext_vec.push(c);
-        }
-
-        let ciphertext_hex: String = ciphertext_vec
-        .iter()
-        .map(|c| {
-            let hex_string = c.to_str_radix(16);
-            if hex_string.len() == 1 {
-                format!("0{}", hex_string)
-            } else {
-                hex_string
-            }
-        })
-        .collect();
-
-        let encoded = general_purpose::STANDARD.encode(&ciphertext_hex);
+        let message = "I Love U So Much!!!";
+        println!("Message: {}", message);
+        let encoded = crypto_utils::encrypt_to_b64(message, &affn);
+        println!("Encoded Message: {}", encoded);
 
         // DEKRIPSI
-        
-        let decoded_bytes = general_purpose::STANDARD.decode(&encoded).unwrap();
-        let decoded_string = String::from_utf8(decoded_bytes).unwrap();
-        let ciphertext = decoded_string;
-
-        println!("\nSebelum Dekripsi: ");
-        println!("Dari Alice: {}", encoded.clone());
-
-        let mut decoded_ciphertext: Vec<BigInt> = Vec::new();
-        for i in (0..ciphertext.len()).step_by(2) {
-            if let Ok(hex_str) = &ciphertext[i..i+2].parse::<String>() {
-                if let Ok(num) = BigInt::from_str_radix(hex_str, 16) {
-                    decoded_ciphertext.push(num);
-                }
-            }
-        }
-
-        let mut plaintext_vec: Vec<BigInt> = vec![];
-        for dc in &decoded_ciphertext {
-            let plain = affn.decrypt(&dc);
-            plaintext_vec.push(plain);
-        }
-
-        let plaintext: String = plaintext_vec
-            .iter()
-            .map(|p| {
-                let string = p.to_str_radix(10);
-                let value = u32::from_str_radix(&string, 10).unwrap();
-                char::from_u32(value).unwrap()
-            })
-            .collect();
-
-        println!("\nHasil Dekripsi");
-        println!("Dari Alice: {}", plaintext);
+        let plaintext = crypto_utils::decrypt_from_b64(&encoded, &mut affn);
+        println!("Plaintext: {}", plaintext);
     }
     else {
         println!("p must be a safe prime");
